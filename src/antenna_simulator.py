Fix issues with the rotation of the earth

src/antenna_simulator.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy
 
 from src.conversions import ecef2llh, rad2semicircles, ecef2aer, semicircles2rad, seconds2day_of_year
-from src.constants import GPS_L1_FREQUENCY
+from src.constants import GPS_L1_FREQUENCY, OMEGA_EARTH
 
 
 class AntennaSimulator:
@@ -175,22 +175,20 @@ class AntennaSimulator:
 
         return dry_delay, wet_delay
 
-
-    def get_pseudoranges(self, satellite_positions_ecef, player_position_ecef, reciever_clock_bias,
-                         time_of_week_gps_seconds: np.float64):
+    def get_pseudoranges(self, satellite_positions_ecef, receiver_position_ecef, reciever_clock_bias, time_of_week_gps_seconds: np.float64):
 
         satellite_amount = satellite_positions_ecef.shape[0]
 
         # TODO compute the receiver clock time
-        ionospheric_delay = self._ionospheric_delay_calculation(satellite_positions_ecef, player_position_ecef,
+        ionospheric_delay = self._ionospheric_delay_calculation(satellite_positions_ecef, receiver_position_ecef,
                                                                 time_of_week_gps_seconds)
 
         day_of_year = seconds2day_of_year(time_of_week_gps_seconds)
-        tropospheric_delay = self._tropospheric_delay_calculation(satellite_positions_ecef, player_position_ecef, day_of_year)
+        tropospheric_delay = self._tropospheric_delay_calculation(satellite_positions_ecef, receiver_position_ecef, day_of_year)
 
         # See GNSS Applications and Methods (GNSS Technology and Applications) section 3.3.1.1
         bias_difference = scipy.constants.c * (reciever_clock_bias - self.satellite_clock_bias.reshape((-1)))
-        range = np.linalg.norm(satellite_positions_ecef - player_position_ecef, axis=1).reshape((-1))
+        range = np.linalg.norm(satellite_positions_ecef - receiver_position_ecef, axis=1).reshape((-1))
 
         # Assume open field
         # From GNSS Applications and Methods (GNSS Technology and Applications) section 3.3.1.1
@@ -215,7 +213,10 @@ class AntennaSimulator:
 
         localNoiseEffect = correction(jammer)
 
-        pseudorange = range + bias_difference + tropospheric_delay + scipy.constants.c * ionospheric_delay + multipath_bias + epsilon + localNoiseEffect
+        pseudorange = (
+            range + bias_difference + tropospheric_delay + scipy.constants.c * ionospheric_delay
+            + multipath_bias + epsilon + localNoiseEffect
+        )
 
         return pseudorange
 

src/gnss_sensor.py
@@ -28,8 +28,8 @@ class GnssSensor:
         print(f"Satelite positions: {satellite_positions_ecef}")
         print(f"Satelite velocities: {satellite_velocities_ecef}")
 
-        # TODO check if satellites are over the horizon
         player_position = player_positions[-1]
+        player_velocity = player_velocities[-1]
 
         time_of_week_gps_seconds = time_gps2seconds_of_week(np.float64(time_gps.to_gpst_seconds()))
 
@@ -59,7 +59,6 @@ class GnssSensor:
         print(f"GDOP {gdop} HDOP {hdop} VDOP {vdop} PDOP {pdop}")
         print(f"Error {np.linalg.norm(position_aproximation-player_position)}m, real position {player_position}m, real reciever clock bias {reciever_clock_bias}s")
 
-        player_velocity = player_velocities[-1]
         direct_doppler = self.simulator.get_doppler(visible_satellite_positions_ecef, satellite_velocities_ecef,
                                                     player_position, player_velocity, reciever_clock_drift)
 

src/gps_satellite.py
@@ -1,8 +1,10 @@
 from typing import Tuple
 
 import numpy as np
-from hifitime import Epoch
+import scipy
+from hifitime import Epoch, Unit
 
+from src.constants import OMEGA_EARTH
 from src.gps_orbital_parameters import GpsOrbitalParameters
 from src.numpy_types import array3d
 from src.conversions import time_gps2seconds_of_week, gps_seconds_wrap, time_gps2week_number, SECONDS_IN_NANOSECONDS
@@ -14,6 +16,43 @@ class GpsSatellite:
 
         if orbit_parameters.sv_clock_drift != 0 or orbit_parameters.sv_clock_drift_rate != 0:
             print(f"WARNING: drift and drift rate parameters are not 0 for satellite {orbit_parameters.prn_number} but clock drift will NOT be modeled!")
+
+    def position_velocity_for_receiver(self, receiver_position_ecef: array3d, arrival_gps_time: Epoch,
+                                       tolerance: np.float64 = 0.1):
+        """
+        Compute the satellite position at the time the signal was sent,
+        for it to be received at position receiver_position_ecef at time gps_time
+        """
+        # ECEF to ECI provided from Principles of GNSS, Inertial, and Multisensor Integrated Navigation Systems section 2.5.1
+        # Algorithm from DESARROLLO E IMPLEMENTACIÓN DE UN SIMULADOR DE DATOS GNSS section 2.3
+
+        estimated_transmission_time_gps = arrival_gps_time
+        error = np.inf
+
+        for _ in range(10):
+            if error < tolerance:
+                break
+            satellite_position_ecef, satellite_velocity_ecef = self.position_velocity(estimated_transmission_time_gps)
+
+            dt = np.linalg.norm(receiver_position_ecef - satellite_position_ecef) / scipy.constants.c
+            theta = OMEGA_EARTH * dt
+
+            ecef2eci_position_matrix = np.array([
+                [np.cos(theta), -np.sin(theta), 0],
+                [np.sin(theta),  np.cos(theta), 0],
+                [0,              0,             1],
+            ], np.float64)
+
+            receiver_position_eci = receiver_position_ecef
+
+            satellite_position_eci = ecef2eci_position_matrix @ (satellite_position_ecef - satellite_velocity_ecef * dt)
+
+            range_approx = np.linalg.norm(satellite_position_eci - receiver_position_eci)
+            estimated_transmission_time_gps = arrival_gps_time - Unit.Second * (range_approx / scipy.constants.c)
+            error = np.linalg.norm(satellite_position_ecef - self.position_velocity(estimated_transmission_time_gps)[0], ord=1)
+
+        return self.position_velocity(estimated_transmission_time_gps)
+
 
     def position_velocity(self, gps_time: Epoch) -> Tuple[array3d, array3d]:
         # From IS-GPS-200M table 20-IV

src/main.py
@@ -2,7 +2,6 @@ import datetime
 import os
 from pathlib import Path
 from typing import List
-import datetime as dt
 
 from hifitime import Epoch, TimeScale, Duration, Unit
 from pyray import *
@@ -63,10 +62,10 @@ CUTOFF_ELEVATION = np.deg2rad(10)
 
 SATELLITE_NUMBER = 6
 
-RECEIVER_CLOCK_BIAS = np.float64(1 * 1e-3)
+RECEIVER_CLOCK_BIAS = np.float64(0)# np.float64(1 * 1e-3)
 # Walk of 0.1us per second
 # From https://www.e-education.psu.edu/geog862/node/1716
-RECEIVER_CLOCK_WALK = np.float64(1 * 1e-6)
+RECEIVER_CLOCK_WALK = np.float64(0)#np.float64(1 * 1e-6)
 
 SATELLITE_NOISE_STD = np.float64(0.0)
 
@@ -166,10 +165,6 @@ def main():
 
         time_since_gnss += delta
 
-        satellite_positions_velocities = np.array([satellite.position_velocity(time_gps) for satellite in cut_satellite_orbits], dtype=np.float64)
-        satellite_positions = satellite_positions_velocities[:, 0, :]
-        satellite_velocities = satellite_positions_velocities[:, 1, :]
-
         if is_key_down(KEY_LEFT) or is_key_down(KEY_A):
             player_delta_x = -1
         elif is_key_down(KEY_RIGHT) or is_key_down(KEY_D):
@@ -190,6 +185,10 @@ def main():
 
         player_velocity = MOVEMENT_SPEED_METERS_PER_SECOND * player_delta
         player_velocities.append(player_velocity)
+
+        satellite_positions_velocities = np.array([satellite.position_velocity_for_receiver(player_position, time_gps) for satellite in cut_satellite_orbits], dtype=np.float64)
+        satellite_positions = satellite_positions_velocities[:, 0, :]
+        satellite_velocities = satellite_positions_velocities[:, 1, :]
 
         if time_since_gnss > 1/GNSS_MESSAGE_FREQUENCY:
             # See GNSS Applications and Methods (GNSS Technology and Applications) section 3.3.1.1