518324e51b1d2f083d93e352b7a126b4f9815957
EmmanuelMess <emmanuelbendavid@gmail.com>
2025-06-18 10:24:49 -0300

    Small fix

--- src/solver.py
+++ src/solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy
 
-from src.conversions import velocity_eci2ecef
+from src.conversions import velocity_eci2ecef, position_ecef2eci
 
 
 class Solver:
@@ -9,7 +9,7 @@ class Solver:
         self.satellite_clock_bias = satellite_clock_bias
         self.satellite_frequencies = satellite_frequencies
 
-    def solve_position(self, satellite_positions_ecef, pseudorange, xtol):
+    def solve_position(self, satellite_positions_eci, pseudorange, xtol):
         """
         A linearization of the pseudorange error, via a taylor aproximation, is minimized.
         But has problems on numerical precision (substracts large floating values)
@@ -17,8 +17,8 @@ class Solver:
         Numeric fixes from https://gssc.esa.int/navipedia/index.php?title=Code_Based_Positioning_(SPS)
         :return:
         """
-        satellite_amount = satellite_positions_ecef.shape[0]
-        gnss_position_aproximation = np.array([1, 1, 1], dtype=np.float64)
+        satellite_amount = satellite_positions_eci.shape[0]
+        position_aproximation_ecef = np.array([1, 1, 1], dtype=np.float64)
         gnss_receiver_clock_bias_approximation = np.float64(1e-6) * scipy.constants.c
         gnss_position_error = np.inf
 
@@ -28,29 +28,29 @@ class Solver:
             if gnss_position_error < xtol:
                 break
 
-            gnss_pseudorange_approximation = (
-                        np.linalg.norm(satellite_positions_ecef - gnss_position_aproximation, axis=1)
+            pseudorange_approximation = (
+                        np.linalg.norm(satellite_positions_eci - position_aproximation_ecef, axis=1)
                         + (gnss_receiver_clock_bias_approximation - self.satellite_clock_bias * scipy.constants.c))
 
-            delta_gnss_pseudorange = pseudorange.copy() - gnss_pseudorange_approximation
+            delta_gnss_pseudorange = pseudorange.copy() - pseudorange_approximation
 
-            delta_satelites = gnss_position_aproximation - satellite_positions_ecef
+            delta_satelites = position_aproximation_ecef - satellite_positions_eci
             cs = np.ones((1, satellite_amount), dtype=np.float64)
 
-            G = np.concatenate((delta_satelites / gnss_pseudorange_approximation.reshape((-1, 1)), cs.T), axis=1)
+            G = np.concatenate((delta_satelites / pseudorange_approximation.reshape((-1, 1)), cs.T), axis=1)
 
             m = (np.linalg.pinv(G) @ delta_gnss_pseudorange.T).reshape(-1)
             gnss_position_delta = m[:3]
             gnss_clock_bias_delta = m[-1]
 
-            gnss_position_aproximation += gnss_position_delta
+            position_aproximation_ecef += gnss_position_delta
             gnss_receiver_clock_bias_approximation += gnss_clock_bias_delta
 
             gnss_position_error = np.linalg.norm(gnss_position_delta)
 
         gnss_receiver_clock_bias_approximation /= scipy.constants.c
 
-        return gnss_position_aproximation, gnss_receiver_clock_bias_approximation, np.linalg.norm(delta_gnss_pseudorange)
+        return position_aproximation_ecef, gnss_receiver_clock_bias_approximation, np.linalg.norm(delta_gnss_pseudorange)
 
 
     def solve_position_scipy(self, satellite_positions_ecef, pseudorange, xtol):
@@ -99,6 +99,8 @@ class Solver:
         From Global Positioning System section 6.2.1, adapted from getGnssPositionTaylor
         """
         # TODO add satelite weighting
+        receiver_position_eci = position_ecef2eci(receiver_position_ecef, 0.0)
+
         satellite_amount = satellite_positions_eci.shape[0]
 
         pseudorange_rates = scipy.constants.c / self.satellite_frequencies * direct_doppler
@@ -114,7 +116,7 @@ class Solver:
                 break
 
             velocity_difference = gnss_velocity_eci_aproximation - satellite_velocities_eci
-            satellite_user_delta = satellite_positions_eci - receiver_position_ecef
+            satellite_user_delta = satellite_positions_eci - receiver_position_eci
             satellite_line_of_sight = satellite_user_delta / np.linalg.norm(satellite_user_delta, axis=1).reshape((-1, 1))
             velocity_scalar_projection = np.sum(velocity_difference * satellite_line_of_sight, axis=1)
             pseudorange_rates_approximation = velocity_scalar_projection + gnss_receiver_clock_drift_approximation
@@ -134,7 +136,6 @@ class Solver:
 
             gnss_velocity_error = np.linalg.norm(gnss_velocity_delta)
 
-        receiver_position_eci = receiver_position_ecef
         gnss_velocity_ecef_aproximation = velocity_eci2ecef(receiver_position_eci, gnss_velocity_eci_aproximation, 0.0)
 
         return gnss_velocity_ecef_aproximation, gnss_receiver_clock_drift_approximation, gnss_velocity_error
